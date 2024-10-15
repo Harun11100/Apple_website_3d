f8e4cfa5585cc355b0e3282fb6c1b10f16358f06
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-10-15 23:08:39 +0600

    Highlight section is done

--- src/component/Videocarosel.jsx
+++ src/component/Videocarosel.jsx
@@ -25,7 +25,14 @@ const Videocarosel = () => {
 
       useGSAP(()=>{
 
- gsap.to('#video',{
+
+      gsap.to('#slider',{
+            transform:`translateX(${-100*videoId}%)`,
+            duration:2,
+            ease:'power2.inOut'
+      })     
+
+      gsap.to('#video',{
       scrollTrigger:{
             trigger:'#video',
             toggleActions:'restart none none none'
@@ -61,6 +68,10 @@ const Videocarosel = () => {
              case 'play':
                    setVideo((pre)=>({...pre,isPlaying :!pre.isPlaying}))
                    break;
+
+             case 'pause':
+                   setVideo((pre)=>({...pre,isPlaying :!pre.isPlaying}))
+                   break;
             default:
                   break ;
       }
@@ -86,24 +97,65 @@ const Videocarosel = () => {
      }
 
      useEffect(()=>{
-        const currentProgress=0;
+        let currentProgress=0;
         let span=videoSpanRef.current;
        
         if(span[videoId]){
-
+       
            let animate=gsap.to(span[videoId],{
 
             onUpdate:()=>{
+               const progress=Math.ceil(animate.progress()*100)
 
+               if (currentProgress!=progress){
+                  currentProgress=progress;
+                  
+                  gsap.to(videoDivRef.current[videoId],{
+                        width:window.innerWidth<760?'10vw': window.innerWidth<1200?'10vw':'4vw'
+                  })
+                  
+                  gsap.to(span[videoId],{
+                        width:`${currentProgress}%`,
+                        backgroundColor:'white'
+                  })
+               }
 
             },
             onComplete:()=>{
 
+                   if(isPlaying){
+                        gsap.to(videoDivRef.current[videoId],{
+                              width:'12px'
+                        })
+
+                        gsap.to(span[videoId],{
+                              backgroundColor:'afafaf'
+                        })
+                   }
 
             }
 
 
            })
+             if(videoId===0){
+                  animate.restart()
+             }
+            
+             const animateUpdate=()=>{
+                  animate.progress(videoRef.current[videoId].currentTime/highlightsSlide[videoId].videoDuration)
+
+
+
+             }
+
+             if(isPlaying){
+                  gsap.ticker.add(animateUpdate)
+        }else{
+            gsap.ticker.remove(animateUpdate)
+        }
+
+
+      
         }
 
      },[videoId,startPlay])
@@ -115,7 +167,18 @@ const Videocarosel = () => {
             <div key={list.id} id="slider" className="sm:pr-20 pr-10">
                  <div className="video-carousel_container">
                     <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
-                            <video id='video'preload='auto'  muted ref={(el)=>(videoRef.current[i]=el)}
+                            <video id='video'preload='auto' className={`${
+                              list.id===2 && 'translate-x-44' } pointer-event-none
+                            `}  
+                            muted ref={(el)=>(videoRef.current[i]=el)}
+                            
+                            onEnded={()=>
+
+                              i!==3 ? handleProcess('video-end',i):handleProcess('video-last')
+
+                            }
+                            
+                            
                             onPlay={()=>{
                               setVideo((prevVideo)=>({
                                     ...prevVideo,isPlaying:true
@@ -123,6 +186,7 @@ const Videocarosel = () => {
                             }
                             }
                             playsInline={true}
+
                             onLoadedMetadata={(e)=>handleLoadedMetadata(i,e)}
                             >
                               <source src={list.video} type='video/mp4'/>
@@ -150,9 +214,7 @@ const Videocarosel = () => {
       <span key={i} ref={(el)=>(videoDivRef.current[i]=el)} className="mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer">
 
               <span className="absolute w-full h-full rounded-full" 
-              ref={(el)=>(videoSpanRef.current[i]=el)}/>
-
-            
+              ref={(el)=>(videoSpanRef.current[i]=el)}/>   
       </span>
      ))}
 
